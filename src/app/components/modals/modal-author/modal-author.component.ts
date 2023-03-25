04aef4992e8d204543a7c782e257417dac018964
robert <78978033+robertGGA@users.noreply.github.com>
2023-03-25 22:19:29 +0300

    added modal author

--- src/app/components/modals/modal-author/modal-author.component.ts
+++ src/app/components/modals/modal-author/modal-author.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {AuthorService} from "@services/author.service";
+import {AuthorModel} from "@models/author-model";
+import {DestroyService} from "@services/destroy.service";
+import {debounceTime, startWith, switchMap, takeUntil} from "rxjs";
 
 @Component({
   selector: 'gp-modal-author',
@@ -7,10 +12,38 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ModalAuthorComponent implements OnInit {
+  authors!: Array<AuthorModel>;
+  form: FormGroup
 
-  constructor() { }
+  constructor(private authorService: AuthorService,
+              private destroy$: DestroyService,
+              private cdr: ChangeDetectorRef,
+              private fb: FormBuilder) {
+    this.form = fb.group({
+      'search': ['']
+    })
+  }
 
   ngOnInit(): void {
+    this.form.valueChanges
+      .pipe(
+        takeUntil(this.destroy$),
+        startWith({search: ''}),
+        debounceTime(300),
+        switchMap(value => this.authorService.searchAuthors(value.search)))
+      .subscribe(value => {
+        this.authors = value;
+        this.cdr.markForCheck();
+      })
+  }
+
+  chooseAuthor(id: number) {
+    console.log(id);
+  }
+
+  removeAuthor(event: Event, id: number) {
+    event.stopPropagation();
+    this.authorService.removeAuthor(id);
   }
 
 }
